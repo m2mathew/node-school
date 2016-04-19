1a0f2c00fe0b3f2b448869100f0d4066b51c861f
Mike Mathew <m2mathew@me.com>
2016-04-19 15:58:50 -0500

    stuck on step 3

--- thinking-in-react/program.js
+++ thinking-in-react/program.js
@@ -1,16 +1,5 @@
 import React from 'react'
 
-export const FilterableProductTable = React.createClass({
-  render() {
-    return (
-      <div>
-      	<h1 style={{ fontFamily: 'Input Mono' }}>O hai, this is a table!</h1>
-      	<SearchBar />
-      </div>
-    )
-  }
-})
-
 export const SearchBar = React.createClass({
 	render() {
 		return (
@@ -23,4 +12,54 @@ export const SearchBar = React.createClass({
 			</form>
 		)
 	}
+})
+
+export const ProductCategoryRow = React.createClass({
+	render() {
+		return (
+			<thead>
+				<tr>
+					<th>A category row</th>
+				</tr>
+			</thead>
+		)
+	}
+})
+
+export const ProductRow = React.createClass({
+	render() {
+		return (
+			<tbody>
+				<tr>
+					<td>A row</td>
+				</tr>
+			</tbody>
+		)
+	}
+})
+
+export const ProductTable = React.createClass({
+	render() {
+		return (
+			<table>
+				<ProductCategoryRow />
+				<ProductRow />
+			</table>
+		)
+	}
+})
+
+
+export const FilterableProductTable = React.createClass({
+  render() {
+    return (
+      <div>
+      	<h1 style={{ fontFamily: 'Input Mono' }}>O hai, this is a table!</h1>
+      	<SearchBar />
+      	<ProductTable
+      		products={this.props.products}
+      	/>
+      </div>
+    )
+  }
 })

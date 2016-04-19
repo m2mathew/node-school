af2c9ad3553dc632209d4d42dd912546bb43148c
Mike Mathew <m2mathew@me.com>
2016-04-19 15:37:53 -0500

    getting closer to the thinking in react

--- thinking-in-react/program.js
+++ thinking-in-react/program.js
@@ -3,7 +3,24 @@ import React from 'react'
 export const FilterableProductTable = React.createClass({
   render() {
     return (
-      <div>o hai!</div>
+      <div>
+      	<h1 style={{ fontFamily: 'Input Mono' }}>O hai, this is a table!</h1>
+      	<SearchBar />
+      </div>
     )
   }
+})
+
+export const SearchBar = React.createClass({
+	render() {
+		return (
+			<form style={{ paddingLeft: '50px' }}>
+				<input type="search" placeholder="Search..."/>
+				<label>
+					<input type="checkbox" />
+					Only show products in stock
+				</label>
+			</form>
+		)
+	}
 })
